61a2486fc20d7b29fcb41035c9b54c1f99ccf617
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-08-04 15:56:56 +0900

    Fix gen.go

--- gen.go
+++ gen.go
@@ -1,6 +1,6 @@
 package biblioseeq
 
 // Generate from module definition.
-//go:generate_input gen-from-mod.go gen_from_mod/*.go go.mod *.gen_from_mod.go.tmpl */*.gen_from_mod.go.tmpl
-//go:generate_output *.gen_from_mod.go */*.gen_from_mod.go
+//go:generate_input gen-from-mod.go gen-from-mod/*.go go.mod *.gen-from-mod.go.tmpl */*.gen-from-mod.go.tmpl
+//go:generate_output *.gen-from-mod.go */*.gen-from-mod.go
 //go:generate go run ./gen-from-mod/run.go
