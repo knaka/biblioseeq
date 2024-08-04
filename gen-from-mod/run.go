3b4c2e3e5805f9bc6dfab28cc7b5c10ae3cc31fc
Kiichiro Kyle NAKA <knaka@ayutaya.com>
2024-08-04 15:42:20 +0900

    Refactor

--- gen-from-mod/run.go
+++ gen-from-mod/run.go
@@ -7,13 +7,15 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 
 	. "github.com/knaka/go-utils"
 )
 
 func main() {
 	cmd := exec.Command("go", "run", ".")
-	cmd.Dir = filepath.Join(V(os.Getwd()), "gen-from-mod")
+	_, thisFilePath, _, _ := runtime.Caller(0)
+	cmd.Dir = filepath.Dir(thisFilePath)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	V0(cmd.Run())
